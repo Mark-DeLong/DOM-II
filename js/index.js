f35792c46040fd743aca116044c12cb8bed69d1c
Mark <mark@Marks-MacBook-Air.local>
2020-12-01 16:52:51 -0500

    Set up scaffolding

--- js/index.js
+++ js/index.js
@@ -1,1 +1,64 @@
-// Your code goes here
+// // mouseover
+// const mouseover = document.querySelector(".nav-link")
+
+// mouseover.addEventListener("mouseover", function (event) {
+//     console.log('mouseover')
+// })
+
+// keydown
+const keydown = document.querySelector(".nav-link")
+keydown.addEventListener("keydown", function (event) {
+    console.log('keydown')
+})
+
+// // wheel
+// const wheel = document.querySelector(".nav-link")
+
+// wheel.addEventListener("wheel", function (event) {
+//     console.log('wheel')
+// })
+
+// load
+const load = document.querySelector(".nav-link")
+
+load.addEventListener("load", function (event) {
+    console.log('load')
+})
+
+// focus
+const focus = document.querySelector(".nav-link")
+
+focus.addEventListener("focus", function (event) {
+    console.log('focus')
+})
+
+// resize
+const resize = document.querySelector(".nav-link")
+
+resize.addEventListener("resize", function (event) {
+    console.log('resize')
+})
+
+// scroll
+const scrollx = document.querySelector(".nav-link")
+
+scrollx.addEventListener("scroll", function (event) {
+    console.log('scroll')
+})
+
+//select
+const select = document.querySelector(".nav-link")
+
+select.addEventListener("select", function (event) {
+    console.log('select')
+})
+
+// // dblclick
+// const dblclick = document.querySelector(".nav-link")
+
+// dblclick.addEventListener("dblclick", function (event) {
+//     console.log('dblclick')
+// })
+
+// // drag / drop 2 events needed const 
+// // drag / drop = document.querySelector("#")
